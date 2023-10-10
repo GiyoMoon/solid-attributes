80d6667dc9ee21886c598ceb3c977132e59153e1
Jasmin <code@jasi.dev>
2023-10-10 23:08:24 +0200

    :sparkles: Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <p one two three four="with value" five six seven>Check the attributes of this element</p>
+    </div>
+  )
+}
+
+export default App
